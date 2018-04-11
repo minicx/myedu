8e9281769e516ac3a614391a5dbf6f7d61036fde
minicx <1404995037@qq.com>
2018-04-11 10:57:50 +0800

    新建项目
    新建项目

---
+++ myedu/src/main/java/com/xcao/myedu/MyeduApplication.java
@@ -0,0 +1,12 @@
+package com.xcao.myedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeduApplication.class, args);
+	}
+}
